e23967ddcafff144986b6038c945def657356c3f
Gael Lemus <gelemus@csu.fullerton.edu>
2023-10-31 09:32:30 -0700

    Made the sandwich.cc file again and made a program in average.cc that calculates the average given command line arguments.

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Gael Lemus
+// gelemus@csu.fullerton.edu
+// @GaelLemus
+// Partners: @giovannyr23
 
 #include <iostream>
 #include <string>
@@ -7,17 +10,23 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that the number of arguments is correct.
+  // XTODO: Validate that the number of arguments is correct.
   // If not, print an error message and return a non-zero value.
-
-  // TODO: Declare three std::string variables to hold the
+  if (argc != 4) {
+    std::cout << "error: you must supply three arguments";
+    return 73;
+  }
+  // XTODO: Declare three std::string variables to hold the
   // protein, bread, and condiment input.
   // Initialize each variable with an element of the arguments vector
   // declared above.
-
-  // TODO: Use cout to print output following the pattern
+  std::string protein{arguments[1]};
+  std::string bread{arguments[2]};
+  std::string condiment{arguments[3]};
+  // XTODO: Use cout to print output following the pattern
   // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
   // on its own line.
-
+  std::cout << "Your order: A " << protein << " sandwich on " << bread
+            << " with " << condiment;
   return 0;
 }
